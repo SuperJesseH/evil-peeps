4b0a65a701b7bcf2817c83fb1f7fa1d72ffd4651
Jesse <Jesse@172-16-239-208.DYNAPOOL.NYU.EDU>
2015-09-17 22:07:15 -0400

    fisrt test

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var gameMode
+var secondCount
+var rateOfPlay
+var peepLocX
+var peepLocY
+var onOff
+
+function setup() {
+  createCanvas(2208 / 2, 1242 / 2) // Half of my phones screen resoulution
+  background(60)
+  gameMode = 1
+  rateOfPlay = 1
+
+
+}
+
+function draw() {
+  peepLocX = mouseX + random(-2, 2)
+  peepLocY = mouseY + random(-2, 2)
+  onOff = random(0,1)
+  
+}
